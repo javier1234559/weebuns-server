ef8d53076662a6367b2590c8df6306811e0ed1b5
OriSanDLL <orisandl2803@gmail.com>
2024-10-16 00:14:34 +0700

    feat(New Update type return for user!): :ambulance: support!
    update content!

--- src/models/user/restful/user.controller.ts
+++ src/models/user/restful/user.controller.ts
@@ -17,6 +17,13 @@ import { UserRole } from 'src/common/type/enum';
 import { CreateUserDto } from 'src/models/user/dtos/create-user.dto';
 import { FindAllUsersDto } from 'src/models/user/dtos/find-all-user.dto';
 import { UpdateUserDto } from 'src/models/user/dtos/update-user.dto';
+import {
+  CreateUserResponse,
+  DeleteUserResponse,
+  UpdateUserResponse,
+  UserResponse,
+  UsersResponse,
+} from 'src/models/user/dtos/user-response.dto';
 import { UserService } from 'src/models/user/user.service';
 
 @Controller('users')
@@ -27,35 +34,42 @@ export class UserController {
   @UseGuards(AuthGuard, RolesGuard)
   @Roles(UserRole.USER)
   @Get()
-  async findAll(@Query() findAllUsersDto: FindAllUsersDto) {
+  async findAll(
+    @Query() findAllUsersDto: FindAllUsersDto,
+  ): Promise<UsersResponse> {
     return this.userService.findAll(findAllUsersDto);
   }
 
   @UseGuards(AuthGuard, RolesGuard)
   @Roles(UserRole.ADMIN)
   @Get(':id')
-  async findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string): Promise<UserResponse> {
     return this.userService.findOne(+id);
   }
 
   @UseGuards(AuthGuard, RolesGuard)
   @Roles(UserRole.ADMIN)
   @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
+  async create(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<CreateUserResponse> {
     return this.userService.create(createUserDto);
   }
 
   @UseGuards(AuthGuard, RolesGuard)
   @Roles(UserRole.ADMIN)
   @Put(':id')
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<UpdateUserResponse> {
     return this.userService.update(+id, updateUserDto);
   }
 
   @UseGuards(AuthGuard, RolesGuard)
   @Roles(UserRole.ADMIN)
   @Delete(':id')
-  async remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string): Promise<DeleteUserResponse> {
     return this.userService.remove(+id);
   }
 }
